4d9271a849235f58d76e17127f69a30e09711619
Lunn <thafm.work@gmail.com>
2025-01-10 10:51:29 +0000

    Update cron job UTC

--- convex/crons.ts
+++ convex/crons.ts
@@ -7,12 +7,17 @@ crons.weekly(
   "Weekly Contest",
   {
     dayOfWeek: "friday",
-    hourUTC: 17, // (9:30am Pacific/10:30am Daylight Savings Pacific)
-    minuteUTC: 30,
+    hourUTC: 14,
+    minuteUTC: 0,
   },
   api.posts.getMostLikedPost
 )
 
 export default crons;
+
+// 14:00–16:00 UTC: Morning Americas (6:00–10:00 AM),
+// Afternoon Europe/Africa (2:00–6:00 PM),
+// Evening Asia (10:00 PM–12:00 AM),
+// Late Night Australia (12:00–2:00 AM next day)
 
 // https://docs.convex.dev/api/classes/server.Crons#weekly
